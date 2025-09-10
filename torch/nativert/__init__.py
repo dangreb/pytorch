e378bf9a2d999223d3a733fccdc6809bc2c90b9d
dolpm <34420038+dolpm@users.noreply.github.com>
2025-09-10 10:40:05 -0700

    [nativert] aoti (#162353)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/162353
    
    att
    
    Test Plan:
    ci
    
    Rollback Plan:
    
    Reviewed By: yiming0416
    
    Differential Revision: D81731425

---
+++ torch/nativert/__init__.py
@@ -0,0 +1,6 @@
+from . import backends
+
+
+__all__ = [
+    "backends",
+]
--- torch/nativert/backends/__init__.py
+++ torch/nativert/backends/__init__.py
@@ -1,4 +1,9 @@
+from .lower_utils import lower_exported_program, package_nativert_with_aoti_delegate
 from .lowered_aoti_module import LoweredBackendModule
 
 
-__all__ = ["LoweredBackendModule"]
+__all__ = [
+    "LoweredBackendModule",
+    "lower_exported_program",
+    "package_nativert_with_aoti_delegate",
+]
